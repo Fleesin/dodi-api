714b518bfef4a593a103a01292f9d7deeddb89fa
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-02-19 13:27:46 -0500

    feat(routes): create login route

--- routes/routes.js
+++ routes/routes.js
@@ -19,3 +19,20 @@ router.post('/register', async (req, res) => {
   const savedUser = await user.save();
   return res.status(200).send({ message: 'User created', id: savedUser._id });
 });
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user)
+    return res.status(400).send({ error: 'Email or password is wrong' });
+
+  // Check if password is valid.
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword)
+    return res.status(400).send({ error: 'Email or password is wrong' });
+
+  return res.status(200).json('Logged in');
+});
+
+module.exports = router;
