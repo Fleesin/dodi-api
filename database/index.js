636a3b603f96b0493a27b4c9ab7df953d36d3de5
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-02-19 12:53:21 -0500

    feat(database): crear conección con la BD con mongoose

---
+++ database/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const { log } = console;
+
+mongoose.set("useFindAndModify", false);
+
+module.exports.connect = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://dodiadm:propet2020@clusterdodi0.88y2o.mongodb.net/dodi?retryWrites=true&w=majority"
+    )
+    .then(() => log("Conectado a Atlas"))
+    .catch((error) => console.log(error));
+};
